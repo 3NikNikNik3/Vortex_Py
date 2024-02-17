d43935dac7dc2d5375fc77f88ddb2c19b8f82251
Nikolay_Zakharov <niknik@niknik>
2024-02-17 21:24:07 +0300

    AddSaveKey

--- project/main/static/js/txt/editTxt.js
+++ project/main/static/js/txt/editTxt.js
@@ -4,6 +4,9 @@ var myCodeMirror = CodeMirror(edit_text, {
   lineNumbers: true
 });
 
+myCodeMirror.setSize(null, window.screen.height - edit_text.getBoundingClientRect().y - 150)
+myCodeMirror.addKeyMap({'Ctrl-S': function(e) {text.value = myCodeMirror.getValue(); SAVE()}})
+
 save.onclick = function() {
     text.value = myCodeMirror.getValue()
     SAVE()
