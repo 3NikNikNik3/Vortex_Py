07a405250c5c4a18a2cb7f3360a78b1c9020378d
Nikolay_Zakharov <niknik@niknik>
2024-02-29 11:48:59 +0300

    AddCopy

--- project/main/static/js/index.js
+++ project/main/static/js/index.js
@@ -1,5 +1,5 @@
 function COPY_TEXT(text) {
-    navigator.clipboard.writeText(text);
+    navigator.clipboard.writeText(text)
     up_text.hidden = false
     setTimeout(function() {up_text.hidden = true}, 1000)
 }
