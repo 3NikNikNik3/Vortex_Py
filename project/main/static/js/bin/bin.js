a38d4c03f6d8508e501a99a91dbe1752896bf89d
Nikolay_Zakharov <niknik@niknik>
2024-03-27 10:43:26 +0300

    AddAdd

--- project/main/static/js/bin/bin.js
+++ project/main/static/js/bin/bin.js
@@ -207,6 +207,17 @@ function DelByte() {
     }
 }
 
+function Go() {
+    let add_count = Number(prompt('На сколько? (можно отрицательный)'))
+    if (add_count.toString() != 'Nan') {
+        for (let i = 0; i < max_; ++i) {
+            let q = (parseInt(document.getElementById('li_' + i.toString()).innerText, 16) + add_count) % 256
+            document.getElementById("li_" + i.toString()).innerText = q.toString(16)
+            document.getElementById("li2_" + i.toString()).innerText = GetChar(q.toString(16))
+        }
+    }
+}
+
 tab.onkeydown = KeyOn
 tab2.onkeydown = KeyOn2
 
